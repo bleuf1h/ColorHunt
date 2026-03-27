37adece8f752d1895fdd4a860e7f7dc8c7983605
bleuf1h <tiara.adames15@gmail.com>
2026-03-27 09:04:47 -0400

    Update RainbowMixer.py

---
+++ RainbowMixer.py
@@ -0,0 +1,35 @@
+import flet as ft 
+#Page setup
+def main(page:ft.Page):
+    page.title = "COLOR HUNT/RAINBOW MIXER"
+
+color_valuetext = ft.Text("RGB:0, 0, 0", size = 30)
+
+def colorupdate(e):
+    red = int(redslider.value)
+    green = int(greenslider.value)
+    blue = int(blueslider.value)
+
+hex_value = f"#{red:02x}{green:02x}{blue:02x}"
+page.bgcolor = hex_value
+
+rgb_text.value = f"RGB({red}, {green}, {blue})"
+page.update()
+
+#Sliders
+
+redslider = ft.Slider(min = 0, max = 255, value = 0, on_change = colorupdate)
+greenslider = ft.Slider(min = 0, max = 255, value = 0, on_change = colorupdate)
+blueslider = ft.Slider(min = 0, max = 255, value = 0, on_change = colorupdate)
+
+#Final page setup
+page.add(
+        ft.Text("Red value"),
+        redslider,
+        ft.Text("Green value"),
+        greenslider,
+        ft.Text("Blue value"),
+        blueslider,
+        rgb_text
+    )
+ft.run(main=main)
